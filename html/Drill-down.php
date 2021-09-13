90806a8daed763a5fbabd0d87b8e4b6a0cb2227b
ToutsopDongmoJordan21 <jordantoutsop472@yahoo.fr>
2021-09-13 14:16:08 +0100

    first commit

---
+++ html/Drill-down.php
@@ -0,0 +1,149 @@
+<?php 
+   
+       try
+    {
+         $bdd = mysqli_connect("localhost","root", "", "oders1");
+    }
+        catch (Exception $e)
+    {
+        die('Erreur : ' . $e->getMessage());
+    }
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" type="image/png" sizes="16x16" href="../plugins/images/favicon.png">
+    <title>Data Warehouse</title>
+    <!-- Bootstrap Core CSS -->
+    <link href="bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Menu CSS -->
+    <link href="../plugins/bower_components/sidebar-nav/dist/sidebar-nav.min.css" rel="stylesheet">
+    <!-- animation CSS -->
+    <link href="css/animate.css" rel="stylesheet">
+    <!-- Custom CSS -->
+    <link href="css/style.css" rel="stylesheet">
+    <!-- color CSS -->
+    <link href="css/colors/default.css" id="theme" rel="stylesheet">
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+    <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+<![endif]-->
+</head>
+
+<body class="fix-header">
+    <!-- ============================================================== -->
+    <!-- Preloader -->
+    <!-- ============================================================== -->
+    <div class="preloader">
+        <svg class="circular" viewBox="25 25 50 50">
+            <circle class="path" cx="50" cy="50" r="20" fill="none" stroke-width="2" stroke-miterlimit="10" />
+        </svg>
+    </div>
+    <!-- ============================================================== -->
+    <!-- Wrapper -->
+    <!-- ============================================================== -->
+    <div id="wrapper">
+        <!-- ============================================================== -->
+        <!-- Topbar header - style you can find in pages.scss -->
+        <!-- ============================================================== -->
+       <?php include "php/navbar.php" ; ?>
+        <!-- ============================================================== -->
+        <!-- End Left Sidebar -->
+        <!-- ============================================================== -->
+        <!-- ============================================================== -->
+        <!-- Page Content -->
+        <!-- ============================================================== -->
+        <div id="page-wrapper">
+            <div class="container-fluid">
+                <div class="row bg-title">
+                    <div class="col-lg-3 col-md-4 col-sm-4 col-xs-12">
+                        <h4 class="page-title">Data Warehouse project</h4>
+                    </div>
+                    <div class="col-lg-9 col-sm-8 col-md-8 col-xs-12">
+                    
+                        <ol class="breadcrumb">
+                            <li><a href="#">Basic table</a></li>
+                        </ol>
+                    </div>
+                    <!-- /.col-lg-12 -->
+                </div>
+                <!-- /row -->
+                <div class="row">
+                    <div class="col-sm-12">
+                        <div class="white-box">
+                            <h3 class="box-title">Drill-Down</h3>
+                            <div class="table-responsive">
+                               <?php 
+   
+                                       try
+                                    {
+                                         $bdd = mysqli_connect("localhost","root", "", "oders1");
+                                    }
+                                        catch (Exception $e)
+                                    {
+                                        die('Erreur : ' . $e->getMessage());
+                                    }
+
+                                    $result = mysqli_query($bdd, "SELECT tblorders.OrderDate , tblorders.ShipCity, tblproducts.ProductName, SUM(tblorderdetails.Quantity) AS Quantity
+                                            FROM tblorders JOIN `tblorderdetails`, tblproducts 
+                                            WHERE  tblorders.OrderID = tblorderdetails.OrderID AND tblorderdetails.ProductID = tblproducts.ProductID 
+                                            GROUP BY tblorders.ShipCity");
+                                    echo '<table class="table table-bordered table-condensed table-body-center">
+                                    <thead>
+                                    <tr class="data-heading">
+                                        <th>OrderDate</th>
+                                        <th>ShipCity</th>
+                                        <th>ProductName</th>
+                                        <th>Quantity</th>                                                                            
+                                    </tr>
+                                    </thead>';  
+                                        
+                                                 while($row=mysqli_fetch_array($result)){
+                                            echo "<tbody>";        
+                                            echo "<tr>";
+                                                 echo "<td><b>".$row['OrderDate']."</td>";
+                                                 echo "<td><b>".$row['ShipCity']."</td>";
+                                                 echo "<td><b>".$row['ProductName']."</td>";
+                                                 echo "<td><b>".$row['Quantity']."</td>";
+                                            echo "</tr>";
+                                            echo "</tbody>";
+                                               }
+                                               echo "</table>";
+                                               mysqli_close($bdd);
+                                        ?> 
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- /.row -->
+            </div>
+            <!-- /.container-fluid -->
+            <footer class="footer text-center"> <ul>
+                            </footer>
+        </div>
+        <!-- /#page-wrapper -->
+    </div>
+    <!-- /#wrapper -->
+    <!-- jQuery -->
+    <script src="../plugins/bower_components/jquery/dist/jquery.min.js"></script>
+    <!-- Bootstrap Core JavaScript -->
+    <script src="bootstrap/dist/js/bootstrap.min.js"></script>
+    <!-- Menu Plugin JavaScript -->
+    <script src="../plugins/bower_components/sidebar-nav/dist/sidebar-nav.min.js"></script>
+    <!--slimscroll JavaScript -->
+    <script src="js/jquery.slimscroll.js"></script>
+    <!--Wave Effects -->
+    <script src="js/waves.js"></script>
+    <!-- Custom Theme JavaScript -->
+    <script src="js/custom.min.js"></script>
+</body>
+
+</html>
---
+++ html/php/navbar.php
@@ -0,0 +1,63 @@
+ <nav class="navbar navbar-default navbar-static-top m-b-0">
+            <div class="navbar-header">
+               
+                <!-- /Logo -->
+                <ul class="nav navbar-top-links navbar-right pull-right">
+                    <li>
+                        <a class="nav-toggler open-close waves-effect waves-light hidden-md hidden-lg"
+                            href="javascript:void(0)"><i class="fa fa-bars"></i></a>
+                    </li>
+                    <li>
+                        <form role="search" class="app-search hidden-sm hidden-xs m-r-10">
+                            <input type="text" placeholder="Search..." class="form-control"> <a href=""><i
+                                    class="fa fa-search"></i></a> </form>
+                    </li>
+                   
+                </ul>
+            </div>
+            <!-- /.navbar-header -->
+            <!-- /.navbar-top-links -->
+            <!-- /.navbar-static-side -->
+        </nav>
+        <!-- End Top Navigation -->
+        <!-- ============================================================== -->
+        <!-- Left Sidebar - style you can find in sidebar.scss  -->
+        <!-- ============================================================== -->
+        <div class="navbar-default sidebar" role="navigation">
+            <div class="sidebar-nav slimscrollsidebar">
+                <div class="sidebar-head">
+                    <h3><span class="fa-fw open-close"><i class="ti-close ti-menu"></i></span> <span
+                            class="hide-menu">Navigation</span></h3>
+                </div>
+                <ul class="nav" id="side-menu">
+                    <li style="padding: 70px 0 0;">
+                        <a href="dashboard.html" class="waves-effect"><i class="fa fa-clock-o fa-fw"
+                                aria-hidden="true"></i>Data Warehouse Project</a>
+                    </li>
+                    <li>
+                        <a href="basic-table.php" class="waves-effect">basic-table</a>
+                    </li>
+                     <li>
+                        <a href="basic-cube.php" class="waves-effect">basic-cube</a>
+                    </li>
+                    <li>
+                        <a href="Roll-up.php" class="waves-effect">Roll-up</a>
+                    </li>
+                    <li>
+                    </li>
+                    <li>
+                        <a href="Drill-down.php" class="waves-effect">Drill-down</a>
+                    </li>
+                    <li>
+                        <a href="Slice.php" class="waves-effect">Slice</a>
+                    </li>
+                    <li>
+                        <a href="Dice.php" class="waves-effect">Dice</a>
+                    </li>
+                    <li>
+                        <a href="Pivot.php" class="waves-effect">Pivot</a>
+                    </li>
+                </ul>
+               
+            </div>
+        </div>
